c8b51b4d28425302631541a3df42fba540923b55
leon-kino <muyelingyin@gmail.com>
2022-08-24 21:32:22 +0900

    index に products をインポート

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Company } from '@/component/template/Company/Company';
 import { Hero } from '@/component/template/Hero/Hero';
 import { PageHeader } from '@/component/template/PageHeader/PageHeader';
 import { Products } from '@/component/template/Products/products';
