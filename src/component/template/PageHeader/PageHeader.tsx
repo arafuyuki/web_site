53fe572313bb03a0cbca198056e6ce7245315860
leon-kino <muyelingyin@gmail.com>
2022-08-24 19:26:05 +0900

    PageHeader 完成

--- src/component/template/PageHeader/PageHeader.tsx
+++ src/component/template/PageHeader/PageHeader.tsx
@@ -1,6 +1,24 @@
 export const PageHeader: React.FC = () => (
-  <div>
-    {/* ここに header を作っていってください！ */}
-    <div></div>
-  </div>
+  <>
+    <div className='mx-auto flex h-[75px] w-11/12 items-center justify-between'>
+      <div className='text-2xl'>Logo</div>
+      <div className='flex w-7/12 justify-around'>
+        <a className='text-lg' href='#home'>
+          home
+        </a>
+        <a className='text-lg' href='#product'>
+          product
+        </a>
+        <a className='text-lg' href='#company'>
+          company
+        </a>
+        <a className='text-lg' href='#access'>
+          access
+        </a>
+        <a className='text-lg' href='#contact'>
+          contact
+        </a>
+      </div>
+    </div>
+  </>
 );
