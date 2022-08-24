a3a5100d190f7cfe13962b6ebac096447b8fe727
arafuyuki <fuyukiara@gmail.com>
2022-08-24 21:06:03 +0900

    company完成しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { PageHeader } from '@/component/template/PageHeader/PageHeader';
+import { Company } from '../component/template/Company/Company';
+
 const Home: React.FC = () => (
-  <main className=''>
-    <PageHeader />
-  </main>
+  <>
+    <main className=''>
+      <PageHeader />
+      <Company />
+    </main>
+  </>
 );
 
 export default Home;
