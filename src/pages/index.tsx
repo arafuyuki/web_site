aa04352df3b86f17b7e6f1add902ec3775b1248d
leon-kino <muyelingyin@gmail.com>
2022-08-24 21:06:23 +0900

    products ページ作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Hero } from '@/component/template/Hero/Hero';
 import { PageHeader } from '@/component/template/PageHeader/PageHeader';
+import { Products } from '@/component/template/Products/products';
 
 const Home: React.FC = () => (
   <main className=''>
     <PageHeader />
     <Hero />
+    <Products />
   </main>
 );
 
