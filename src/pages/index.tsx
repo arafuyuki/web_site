eec27d4d5eb1be996ceb581bba4b7dd7fdb6a1d9
leon-kino <muyelingyin@gmail.com>
2022-08-24 19:49:09 +0900

    Hero を完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { Hero } from '@/component/template/Hero/Hero';
 import { PageHeader } from '@/component/template/PageHeader/PageHeader';
+
 const Home: React.FC = () => (
   <main className=''>
     <PageHeader />
+    <Hero />
   </main>
 );
 
